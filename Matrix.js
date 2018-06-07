2a7e891dc26402cc709861fb4c48417d56d6f0c2
Yusof Bandar <ybandar1902@gmail.com>
2018-06-07 15:44:21 +0100

    printWorld

--- Matrix.js
+++ Matrix.js
@@ -11,8 +11,29 @@ var world = [
 
 function setup(){
 
+    createCanvas(1000, 1000);
+    frameRate(5)
+    printWorld(world, worldOffset);
+
 }
 
 function draw(){
     
+}
+
+function printWorld(world, offset) {
+    var yLength = world.length;
+
+    var xStart = offset - LEDMatrixWidth;
+    var xEnd = offset;
+
+    row = "";
+    for (var y = 0; y < yLength; y++) {
+        for (var x = xStart; x < xEnd; x++) {
+            row += world[y][x];
+        }
+
+        console.log(row);
+        row = "";
+    }
 }
